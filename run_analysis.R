2648d7e3867702dae56af77cc77c68e4343bddb0
Neill Butcher <butcher.neill@gmail.com>
2015-08-12 02:19:09 -0700

    Slight Code Cleanup

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,9 @@
 ##  Coursera Getting and Cleaning Data Course Project
 ##
-##  Accesses the Human Activity Recognition Using Smartphones Dataset (UCI HAR) and performs
-
-##
-##  
+##  Accesses the Human Activity Recognition Using Smartphones Dataset (UCI HAR) 
 ##
 ##  The dataset was downloaded and unzipped before processing.  The file was accessed at:
 ##  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-##
-##  Citation: Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human
-##  Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. 
-##  International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012.
 ##
 ##
 ##  Read the raw dataset from local "UCI_HAR_Datase" folder
